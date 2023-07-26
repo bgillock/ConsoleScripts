27ed19eaff43069681ebebd7df3813443f87b43c
bgillock <bitrate27@gmail.com>
2023-07-26 06:31:21 -0500

    Run on 8080

--- Diff/app.js
+++ Diff/app.js
@@ -6,4 +6,4 @@ const server = http.createServer((req, res) => {
   fs.createReadStream('index.html').pipe(res)
 })
 
-server.listen(process.env.PORT || 3000)
+server.listen(process.env.PORT || 8080)
